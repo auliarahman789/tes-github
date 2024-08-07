95c1d0f827b7cfadd2c74881e1b7f854fa0ca1db
KevinWijaya16 <kevinwijaya161207@gmail.com>
2024-08-07 10:18:28 +0700

    Update halaman home div dan p

---
+++ src/app/kevin/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="text-lg font-semibold bg-slate-500">
+      Kevin
+      <div className="text-sm text-blue-400">
+        <p>Saya sedang mengerjakan project menggunakan Github</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
