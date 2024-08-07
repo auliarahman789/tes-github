804e5d0d8ca48d64674a86006e5ba3fa9bd41033
RioRivaldi <rivaldirio090@gmail.com>
2024-08-07 10:18:20 +0700

    update halaman

---
+++ src/app/riorivaldi/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='text-xl font-extrabold bg-slate-900'>
+      RioRivaldi
+      <p className='text-lg text-red-800'>
+        tes github
+      </p>
+    </div>
+  )
+}
